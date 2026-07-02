62af5b1fce149abb9aa9db757aaed8068e9f468d
Ioannis Petropoulos <johnaipeia@gmail.com>
2026-07-02 19:40:33 +0300

    Fixed compile issue.

--- Humble2/src/Platform/MacOS/MacOSPlatformManager.cpp
+++ Humble2/src/Platform/MacOS/MacOSPlatformManager.cpp
@@ -6,9 +6,11 @@ namespace HBL2
 {
     void MacOSPlatformManager::Initialize()
     {
+#ifdef HBL2_PLATFORM_MACOS
         m_AppDataDirectory = MacOSUtils::GetAppSupportDir();
         m_ResourcesDirectory = MacOSUtils::GetResourcesDir();
         m_ExecutableDirectory = MacOSUtils::GetExecutableDir();
+#endif
     }
 
     void MacOSPlatformManager::Shutdown()
